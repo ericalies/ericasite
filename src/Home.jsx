64cb86b5ccddd24888ee5e3f4aa422a82aa66591
limbalrings <wyatt.tall@wpengine.com>
2024-11-30 17:10:58 -0600

    fixing humor and home columns

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,29 +3,17 @@ import './App.css'
 function Home() {
     return (
         <>
-           
-            <div className="h-4"></div>
-            <blockquote>
-                        Erica Lies is a funny writer and a world-renowned curly-hairologist.
-                     </blockquote>
-            <div className="mx-auto max-w-xl">
-                
-                   
-
-                     <br></br>
-                     <br></br>
-
-                     <img class="float-left ..." src="/src/assets/splits1.png" alt="A woman with curly hair wearing a polka dotted shirt as the sits on the floor and smiles at the camera"/>
-                
-                <div className="p-5">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-blue-900">Muppetational</h5>
-                    <p className="mb-3 font-normal text-gray-700">Hi! I'm a Jim Henson creation come to life.</p>
+            <div className="columns-2 mt-40">
+                <img
+                    className="max-w-lg"
+                    src="/src/assets/splits1.png"
+                     alt="A woman with curly hair wearing a polka dotted shirt as the sits on the floor and smiles at the camera"/>
+                <div className="h-screen flex items-center">
+                    <blockquote className="align-middle">
+                        Erica Lies<br/>is a funny writer and a world-renowned curly-hairologist.
+                    </blockquote>
                 </div>
             </div>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, animi voluptas voluptatem dolorum aut cupiditate molestiae quisquam consequatur distinctio accusantium? Expedita doloribus delectus laudantium sint ab nulla eos alias perspiciatis!
-                    
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ipsum quisquam quia architecto itaque, dolorum ullam dolores, enim, quis et cupiditate. Perspiciatis et velit eum est, beatae amet sequi odio! 
-                </p>
         </>
     )
 }
