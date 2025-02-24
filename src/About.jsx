816c5654a4d10cd49096847435137d7810160cb2
wyatt tall <wyatttall@Mac.lan>
2025-02-24 14:18:33 -0500

    deploy now changes

--- src/About.jsx
+++ src/About.jsx
@@ -4,20 +4,26 @@ import './App.css'
 function About() {
     return (
         <>
-       
-            <blockquote className="align-middle"><h1 className='montserrat-alternates-regular'>About Erica</h1></blockquote>
+
+            <blockquote className="align-middle"><h1 className='montserrat-alternates-regular'>About Erica</h1>
+            </blockquote>
             <br></br>
             <div className="card">
-            <p>Erica Lies is a satirist and journalist whose work has appeared in The New Yorker, The New York Times, Vulture, Bust Magazine, McSweeney's Internet Tendency, the Washington Post, and many more. 
-            She's won numerous script awards, is a five-time Second Rounder in the Austin Film Festival, and is a long-time producer for live storytelling phenomenon, Mortified. She's taught and performed improv 
-            comedy for more than a decade and appeared in improv festivals across the country.</p> 
-            <p>Erica has two theater degrees, a mountain of sketch comedy experience, and an annual income of seven drink tickets. 
-           You can read more of her writing by Googling her, but good luck with that. Her last name is a verb.</p>
-           <p>
-            Erica is represented by Laura Mazer at <a href="https://www.wsherman.com/" className='styled-link'>Wendy Sherman Associates Literary Managment</a>.
-        </p>
+                <p>Erica Lies is a satirist and journalist whose work has appeared in The New Yorker, The New York
+                    Times, Vulture, Bust Magazine, McSweeney's Internet Tendency, the Washington Post, and many more.
+                    She's won numerous script awards, is a five-time Second Rounder in the Austin Film Festival, and is
+                    a long-time producer for live storytelling phenomenon, Mortified. She's taught and performed improv
+                    comedy for more than a decade and appeared in improv festivals across the country.</p>
+                <p>Erica has two theater degrees, a mountain of sketch comedy experience, and an annual income of seven
+                    drink tickets.
+                    You can read more of her writing by Googling her, but good luck with that. Her last name is a
+                    verb.</p>
+                <p>
+                    Erica is represented by Laura Mazer at <a href="https://www.wsherman.com/" className='styled-link'>Wendy
+                    Sherman Associates Literary Managment</a>.
+                </p>
             </div>
-            
+
         </>
     )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,22 @@
 import './App.css'
+import betterMuppetURL from './assets/betterMuppet.PNG'
 
 function Home() {
     return (
         <>
-        <blockquote className="montserrat-alternates-bold align-middle"><h1 className='montserrat-alternates-regular'>Erica Lies Has a Home (Page)</h1></blockquote>
+            <blockquote className="montserrat-alternates-bold align-middle"><h1
+                className='montserrat-alternates-regular'>Erica Lies Has a Home (Page)</h1></blockquote>
             <div className="columns-2 mt-10">
                 <img
                     className="max-w-s size-90"
-                    src="/src/assets/ericabio.JPG"
-                     alt="A woman with curly hair wearing a polka dotted shirt sits on the floor and smiles at the camera. 
+                    src={betterMuppetURL}
+                    alt="A woman with curly hair wearing a polka dotted shirt sits on the floor and smiles at the camera. 
                      Her vibe is best described as muppet."/>
-            
+
                 <div className="h-screen flex items-center">
                     {/* <blockquote className="montserrat-alternates-semibold align-middle mb-[80px]"> */}
-                        
-                        <h3 className='montserrat-alternates-semibold mb-[200px]'>
+
+                    <h3 className='montserrat-alternates-semibold mb-[200px]'>
                         Erica is a funny writer who always tells the truth (even when she doesn't).</h3>
                     {/* </blockquote> */}
                 </div>
